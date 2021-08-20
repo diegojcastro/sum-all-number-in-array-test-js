c85e33ab31f0fdeb952368a86b796d46e6548c2f
Diego Castro <diegojcr4@gmail.com>
2021-08-20 15:07:15 -0400

    added sumItems function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let total = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      total += sumItems(item);
+    } else total += item;
+  }
+  return total;
 }
 
 module.exports = sumItems;
